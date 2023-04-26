bb62445804adce21abb4b915c0e3c7a5d9d90e34
ismailcaner <126804265+ismailcaner@users.noreply.github.com>
2023-04-26 19:11:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,43 @@
-self.addEventListener('install', function () {
-    console.log('Install!');
-});
-self.addEventListener("activate", event => {
-    console.log('Activate!');
-});
-self.addEventListener('fetch', function (event) {
-    console.log('Fetch!', event.request);
+import swURL from 'sw:https://ortalamahesap.vercel.app/service-worker.js';
+
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  // Wait for the 'load' event to not block other work
+  window.addEventListener('load', async () => {
+    // Try to register the service worker.
+    try {
+      const reg = await navigator.serviceWorker.register(swURL);
+      console.log('Service worker registered! 😎', reg);
+    } catch (err) {
+      console.log('😥 Service worker registration failed: ', err);
+    }
+  });
+}
+
+// Choose a cache name
+const cacheName = 'cache-v1';
+// List the files to precache
+const precacheResources = ['/', '/index.html', '/css/style.css', '/js/main.js', '/js/app/editor.js', '/js/lib/actions.js'];
+
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
 });
 
-importScripts('https://ortalamahesap.vercel.app/sw/amp-sw.js');
-AMP_SW.init({
-assetCachingOptions: [{
-regexp: /\.(png|jpg)/,
-cachingStrategy: 'CACHE_FIRST'
-}],
-offlinePageOptions: {
-url: '/offline.html',
-assets: []
-}
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
+  );
 });

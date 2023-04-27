0b6a1150e6fcc62db6a45398f1233bc9fec94b11
ismailcaner <126804265+ismailcaner@users.noreply.github.com>
2023-04-27 13:56:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,51 @@
-self.addEventListener('install', e => console.log('pwa installed.'));
-self.addEventListener('fetch', event => {});
+// Servis işçisi dosyası
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/scripts/main.js',
+  '/images/logo.png'
+];
+
+// Önbelleğe alma işlemini gerçekleştirir
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Önbellek açıldı');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Önbelleğe isteği yanıtlar
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          console.log('İstek önbellekten alındı: ', event.request.url);
+          return response;
+        }
+
+        console.log('Ağdan istek yapıldı: ', event.request.url);
+        return fetch(event.request);
+      })
+  );
+});
+
+// Eski önbellekleri temizler
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName !== CACHE_NAME;
+        }).map(function(cacheName) {
+          console.log('Eski önbellek temizlendi: ', cacheName);
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});

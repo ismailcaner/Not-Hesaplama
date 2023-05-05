d3a87e0e955782bf8369106bdf4bf64b4881139a
ismailcaner <126804265+ismailcaner@users.noreply.github.com>
2023-05-06 00:43:26 +0300

    Create button.js

---
+++ button.js
@@ -0,0 +1,12 @@
+function clear() {
+  var elements = document.getElementsByClassName("sonuc");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].value = "";
+    elements[i].innerHTML = "";
+  }
+  var elements = document.getElementsByClassName("input");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].value = "";
+    elements[i].innerHTML = "";
+  }
+}

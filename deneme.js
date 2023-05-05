f4cca8b50b4cd173096aa7dbc376c7fae06c4b18
ismailcaner <126804265+ismailcaner@users.noreply.github.com>
2023-05-05 13:45:38 +0300

    Update deneme.js

--- deneme.js
+++ deneme.js
@@ -1,6 +1,6 @@
 function clear() {
 			document.getElementById("icerik").innerHTML = "";
-			document.getElementsByTagName("input").value = "";
+			document.getElementsById("vrtvize").value = "";
 		}
 
 

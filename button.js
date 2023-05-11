630ca51557037c9c87d09e40a6d79b16f73a99bc
ismailcaner <126804265+ismailcaner@users.noreply.github.com>
2023-05-11 23:00:27 +0300

    Update button.js

--- button.js
+++ button.js
@@ -1,3 +1,4 @@
+//clear butonu
 function clearElements() {
   var elements = document.getElementsByClassName("sonuc");
   for (var i = 0; i < elements.length; i++) {
@@ -9,4 +10,27 @@ function clearElements() {
     elements[i].value = "";
     elements[i].innerHTML = "";
   }
+}
+
+//ders ekleme butonu
+var cloneCounter = 1;
+
+function add() {
+    var divContainer = document.getElementById('lesson');
+    var klon = divContainer.cloneNode(true);
+
+    klon.setAttribute('id', 'lesson' + cloneCounter);
+
+    var orijinalOgeler = divContainer.getElementsByTagName('*');
+    var klonOgeler = klon.getElementsByTagName('*');
+
+    for (var i = 0; i < orijinalOgeler.length; i++) {
+        var orijinalOge = orijinalOgeler[i];
+        var klonOge = klonOgeler[i];
+        klonOge.setAttribute('id', klonOge.getAttribute('id') + '-' + cloneCounter);
+    }
+
+    document.getElementById('lessonarea').appendChild(klon);
+
+    cloneCounter++;
 }

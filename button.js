c36b8bdf5f6edbf19158912fa6df753bda5c0702
ismailcaner <126804265+ismailcaner@users.noreply.github.com>
2023-05-15 00:37:59 +0300

    Update button.js

--- button.js
+++ button.js
@@ -14,44 +14,44 @@ function clearElements() {
 
 //kaydetme btn
 
-const inputs = document.querySelectorAll('input');
-const spans = document.querySelectorAll('span');
-const saveButton = document.getElementById('saveButton');
+document.addEventListener('DOMContentLoaded', function() {
+  const inputs = document.querySelectorAll('input');
+  const spans = document.querySelectorAll('span');
+  const saveButton = document.getElementById('saveButton');
 
-// Sayfa yüklendiğinde verileri kontrol et ve yükle
-window.addEventListener('DOMContentLoaded', function() {
+  // Sayfa yüklendiğinde verileri kontrol et ve yükle
   loadSavedData();
-});
 
-// Verileri yükleme işlemi
-function loadSavedData() {
-  const savedData = localStorage.getItem('savedData');
+  // Verileri yükleme işlemi
+  function loadSavedData() {
+    const savedData = localStorage.getItem('savedData');
 
-  if (savedData) {
-    const data = JSON.parse(savedData);
+    if (savedData) {
+      const data = JSON.parse(savedData);
+
+      inputs.forEach(function(input, index) {
+        const inputKey = `input${index+1}`;
+
+        if (inputKey in data) {
+          input.value = data[inputKey];
+          spans[index].textContent = data[inputKey];
+        }
+      });
+    }
+  }
+
+  // Verileri kaydetme işlemi
+  saveButton.addEventListener('click', function() {
+    const data = {};
 
     inputs.forEach(function(input, index) {
       const inputKey = `input${index+1}`;
-
-      if (inputKey in data) {
-        input.value = data[inputKey];
-        spans[index].textContent = data[inputKey];
-      }
+      data[inputKey] = input.value;
+      spans[index].textContent = input.value;
     });
-  }
-}
 
-// Verileri kaydetme işlemi
-saveButton.addEventListener('click', function() {
-  const data = {};
+    localStorage.setItem('savedData', JSON.stringify(data));
 
-  inputs.forEach(function(input, index) {
-    const inputKey = `input${index+1}`;
-    data[inputKey] = input.value;
-    spans[index].textContent = input.value;
+    alert('Veriler kaydedildi.');
   });
-
-  localStorage.setItem('savedData', JSON.stringify(data));
-
-  alert('Veriler kaydedildi.');
 });

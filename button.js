e2a413f630262093498a60c311688965e7c20808
ismailcaner <126804265+ismailcaner@users.noreply.github.com>
2023-06-23 23:31:01 +0300

    Update button.js

--- button.js
+++ button.js
@@ -11,3 +11,29 @@ function clearElements() {
     elements[i].innerHTML = "";
   }
 }
+
+//save butonu
+ // Önbelleğe kaydetme işlemi
+    function kaydet() {
+      var deger1 = document.getElementById('vize').value;
+      var deger2 = document.getElementById('final').value;
+      
+      // Verileri önbelleğe kaydetme
+      localStorage.setItem('veri1', deger1);
+      localStorage.setItem('veri2', deger2);
+    }
+
+    // Yükleme işlemi
+    window.onload = function() {
+      var veri1 = localStorage.getItem('veri1');
+      var veri2 = localStorage.getItem('veri2');
+      
+      // Kaydedilen verileri yükleme
+      if (veri1 !== null) {
+        document.getElementById('input1').value = veri1;
+      }
+      
+      if (veri2 !== null) {
+        document.getElementById('input2').value = veri2;
+      }
+    }

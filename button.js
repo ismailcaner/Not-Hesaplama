8cfa640303fe0f13edd478889267c2120e83aeab
ismailcaner <126804265+ismailcaner@users.noreply.github.com>
2023-06-24 00:01:42 +0300

    Update button.js

--- button.js
+++ button.js
@@ -1,35 +1,33 @@
 //clear butonu
 function clearElements() {
   var elements = document.getElementsByClassName("sonuc");
-  for (var i = 0; i < elements.length; i++) {
+  {
     elements[i].value = "";
     elements[i].innerHTML = "";
   }
-  var elements = document.getElementsByClassName("input");
-  for (var i = 0; i < elements.length; i++) {
+  var elements = document.getElementsById("vize");
+  {
+    elements[i].value = "";
+    elements[i].innerHTML = "";
+  }
+    var elements = document.getElementsById("final");
+  {
     elements[i].value = "";
     elements[i].innerHTML = "";
   }
 }
 
 //save butonu
- // Önbelleğe kaydetme işlemi
     function kaydet() {
       var deger1 = document.getElementById('vize').value;
       var deger2 = document.getElementById('final').value;
-      
-      // Verileri önbelleğe kaydetme
       localStorage.setItem('veri1', deger1);
       localStorage.setItem('veri2', deger2);
     }
 
-    // Yükleme işlemi
     window.onload = function() {
       var veri1 = localStorage.getItem('veri1');
       var veri2 = localStorage.getItem('veri2');
-      
-      // Kaydedilen verileri yükleme
-    
         document.getElementById('vize').value = veri1;
         document.getElementById('final').value = veri2;
       

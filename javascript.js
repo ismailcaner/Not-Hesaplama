e8ee4a7c90c23bf75ac36a8aef7e3302256b6c9b
ismailcaner <126804265+ismailcaner@users.noreply.github.com>
2023-07-17 15:10:32 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,13 @@
 function hesapla() {
-    var vize1 = document.getElementById("vrtvize").value;
-    var final1 = document.getElementById("vrtfinal").value;
+    var vize = document.getElementById("dersvize").value;
+    var final = document.getElementById("dersfinal").value;
     var yuzdevize = document.getElementById("vize").value;
     var yuzdefinal = document.getElementById("final").value;
-    var yuzde1 = (vize1 * yuzdevize) / 100;
-    var yuzde2 = (final1 * yuzdefinal) /100;
+    var yuzde1 = (vize * yuzdevize) / 100;
+    var yuzde2 = (final * yuzdefinal) /100;
     var sonuc = yuzde1 + yuzde2;
     var sonuc = sonuc.toFixed(1);
-    document.getElementById("conclusion1").innerHTML = sonuc;
+    document.getElementById("sonuc").innerHTML = sonuc;
       }
 
     function preventScroll(event) {

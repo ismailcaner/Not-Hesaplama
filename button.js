f33606c2b1920b3c290c67678d18d2f2119024ef
ismailcaner <126804265+ismailcaner@users.noreply.github.com>
2023-08-12 21:44:22 +0300

    Update button.js

--- button.js
+++ button.js
@@ -18,33 +18,5 @@ function temizle() {
       var veri2 = localStorage.getItem('veri2');
         document.getElementById('vize').value = veri1;
         document.getElementById('final').value = veri2;
-    }
-
-  document.addEventListener("DOMContentLoaded", function() {
-    var girisAlan = document.getElementById("final");
-    var buton = document.getElementById("save");
-    var bildirimContainer = document.getElementById("bildirimContainer");
-    var keyboardIsVisible = false;
-
-    // Giriş alanına odaklandığında
-    girisAlan.addEventListener("focus", function() {
-      keyboardIsVisible = true;
-    });
-
-    // Giriş alanından çıkıldığında
-    girisAlan.addEventListener("blur", function() {
-      keyboardIsVisible = false;
-    });
-
-    // Butona tıklama olayı
-    buton.addEventListener("click", function() {
-      if (keyboardIsVisible) {
-        girisAlan.focus();
-      }
-      bildirimContainer.style.display = "flex";
       
-      setTimeout(function() {
-        bildirimContainer.style.display = "none";
-      }, 2000);
-    });
-  });
+    }

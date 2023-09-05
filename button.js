2702786aea70ae83100957d8a074036817b2a1d6
ismailcaner <126804265+ismailcaner@users.noreply.github.com>
2023-09-05 23:49:27 +0300

    Update button.js

--- button.js
+++ button.js
@@ -1,5 +1,7 @@
 function temizle() {
 //not ve sonuç kısımlarını temizleme işlemi
+
+  location.reload();
   document.getElementById('sonuc').innerHTML = '';
   document.getElementById('dersvize').value = '';
   document.getElementById('dersfinal').value = '';

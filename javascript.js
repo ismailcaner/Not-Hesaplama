fca625183ddb1b912978e065cbaf7aace2374b6a
ismailcaner <126804265+ismailcaner@users.noreply.github.com>
2023-09-12 12:12:24 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -111,4 +111,30 @@ function kaydet() {
          {event.preventDefault();}
          document.addEventListener('touchmove', preventScroll, { passive: false });
 }
+
+
+
+
+function hesapla() {
+//girilen değer yok ise sonuç ekranını tamamen temizleme işlemi
+    var input1 = document.getElementById('dersvize');
+    var input2 = document.getElementById('dersfinal');
+    var sonuc = document.getElementById('sonuc');
+  
+    var vize = document.getElementById("dersvize").value;
+    var final = document.getElementById("dersfinal").value;
+    var yuzdevize = document.getElementById("vize").value;
+    var yuzdefinal = document.getElementById("final").value;
+    var yuzde1 = (vize * yuzdevize) / 100;
+    var yuzde2 = (final * yuzdefinal) /100;
+    var sonuc = yuzde1 + yuzde2;
+    var sonuc = sonuc.toFixed(1);
+    document.getElementById("sonuc").innerHTML = sonuc;
+    
+    if( sonuc < 60 ){
+      sonuc.style.border = 1px solid red;
+}
+
+
+
           

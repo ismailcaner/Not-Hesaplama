ac42d70d8c41725c3a0e7387886e65b2803d3068
ismailcaner <126804265+ismailcaner@users.noreply.github.com>
2023-10-26 01:06:36 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,177 +1,76 @@
-//not kat sayısı kaydetme ve hesaplama işlemi
-function kaydet() {
-    // yalnızca 1 ile 100 arası değer girişine izin verme işlemi
-            var input = document.getElementById("vize");
-            var value = input.value.trim();
-            
-            if (value !== "") {
-                if (parseInt(value) < 1) {
-                    input.value = "";
-                } else if (parseInt(value) > 100) {
-                    input.value = value.slice(0, -1);
-                }
-            }
-          
-            var input = document.getElementById("final");
-            var value = input.value.trim();
-            
-            if (value !== "") {
-                if (parseInt(value) < 1) {
-                    input.value = "";
-                } else if (parseInt(value) > 100) {
-                    input.value = value.slice(0, -1);
-                }
-            }
-    //girilen değer yok ise sonuç ekranını tamamen temizleme işlemi
-            var input1 = document.getElementById('vize');
-            var input2 = document.getElementById('final');
-            var sonuc = document.getElementById('sonuc');
-          
-            if (input1.value || input2.value) {
-            var vize = document.getElementById("dersvize").value;
-            var final = document.getElementById("dersfinal").value;
-            var yuzdevize = document.getElementById("vize").value;
-            var yuzdefinal = document.getElementById("final").value;
-            var yuzde1 = (vize * yuzdevize) / 100;
-            var yuzde2 = (final * yuzdefinal) /100;
-            var sonuc = yuzde1 + yuzde2;
-            var sonuc = sonuc.toFixed(1);
-            document.getElementById("sonuc").innerHTML = sonuc;
+const dizi = ["vize", "final", "dersvize", "dersfinal", "duzenleDersvize", "duzenleDersfinal"];
 
-            document.getElementById("sonuc").style.color = sonuc < 60 ? 'red' : 'black';
-          
-            } else {
-                sonuc.textContent = "";
-            } 
-//girilen değerleri önbelleğe kaydetme işlemi
-            var deger1 = document.getElementById('vize').value;
-            var deger2 = document.getElementById('final').value;
-            localStorage.setItem('veri1', deger1);
-            localStorage.setItem('veri2', deger2);
-            keyboardIsVisible = true;
+function sifirYuz() {
+    dizi.forEach(function (elementID) {
+        var input = document.getElementById(elementID);
+             if (input) {
+                var value = input.value.trim();
+                if (value !== "") {
+                    if (parseInt(value) < 1) {
+                        input.value = "";
+                    } else if (parseInt(value) > 100) {
+                        input.value = value.slice(0, -1);
+                    }
+                }
+            }
+        });
+    }
+
+function islemler() {
+    var input1 = document.getElementById('vize');
+    var input2 = document.getElementById('final');
+    var sonuc = document.getElementById('sonuc');
+    if (input1.value || input2.value) {
+    var vize = document.getElementById("dersvize").value;
+    var final = document.getElementById("dersfinal").value;
+    var yuzdevize = document.getElementById("vize").value;
+    var yuzdefinal = document.getElementById("final").value;
+    var yuzde1 = (vize * yuzdevize) / 100;
+    var yuzde2 = (final * yuzdefinal) /100;
+    var sonuc = yuzde1 + yuzde2;
+    var sonuc = sonuc.toFixed(1);
+    document.getElementById("sonuc").innerHTML = sonuc;
+    document.getElementById("sonuc").style.color = sonuc < 60 ? 'red' : 'black';
+
+    } else {
+        sonuc.textContent = "";
+    } 
 }
 
-//vize final notu hesaplama işlemi
-function hesapla() {
+function cache(){
+    var deger1 = document.getElementById('vize').value;
+    var deger2 = document.getElementById('final').value;
+    localStorage.setItem('veri1', deger1);
+    localStorage.setItem('veri2', deger2);
+    keyboardIsVisible = true;
+}
+//------------üst kısaltma fonksiyonlar------------//
+function kaydet() {
+     sifirYuz()
+     islemler()
+     cache()
+}
 
+function hesapla() {
     var input = document.getElementById("dersvize");
     var input1 = document.getElementById("dersfinal");
-        var value = input.value.trim();
-        var value1 = input1.value.trim();
-
-        if ( value !=="" || value1 !=="") {
+        if ( input !=="" || input1 !=="") {
             openpop.style.opacity = 1;}
-            else if ( value === "" || value1 === ""){
+            else if ( input === "" || input1 === ""){
                 openpop.style.opacity =0.2;
             }
-      // yalnızca 1 ile 100 arası değer girişine izin verme işlemi
-        var input = document.getElementById("dersvize");
-        var value = input.value.trim();
-        
-        if (value !== "") {
-            if (parseInt(value) < 1) {
-                input.value = "";
-            } else if (parseInt(value) > 100) {
-                input.value = value.slice(0, -1);
-            }
-    }
-      
-        var input = document.getElementById("dersfinal");
-        var value = input.value.trim();
-        
-        if (value !== "") {
-            if (parseInt(value) < 1) {
-                input.value = "";
-            } else if (parseInt(value) > 100) {
-                input.value = value.slice(0, -1);
-            }
-    }
-    //girilen değer yok ise sonuç ekranını tamamen temizleme işlemi
-        var input1 = document.getElementById('dersvize');
-        var input2 = document.getElementById('dersfinal');
-        var sonuc = document.getElementById('sonuc');
-      
-        if (input1.value || input2.value) {
-        var vize = document.getElementById("dersvize").value;
-        var final = document.getElementById("dersfinal").value;
-        var yuzdevize = document.getElementById("vize").value;
-        var yuzdefinal = document.getElementById("final").value;
-        var yuzde1 = (vize * yuzdevize) / 100;
-        var yuzde2 = (final * yuzdefinal) /100;
-        var sonuc = yuzde1 + yuzde2;
-        var sonuc = sonuc.toFixed(1);
-        document.getElementById("sonuc").innerHTML = sonuc;
-        
-        document.getElementById("sonuc").style.color = sonuc < 60 ? 'red' : 'black';
-        clear.style.opacity = 1;
 
-        
-    
-        } else {
-            clear.style.opacity= 0.5;
-            sonuc.textContent = "";
-        }
+    sifirYuz()
+    islemler()
     }
 
-
-    function duzenleHesap() {
-
-        var input = document.getElementById("duzenleDersvize");
-        var input1 = document.getElementById("duzenleDersfinal");
-            var value = input.value.trim();
-            var value1 = input1.value.trim();
-    
-         
-          // yalnızca 1 ile 100 arası değer girişine izin verme işlemi
-            var input = document.getElementById("duzenleDersvize");
-            var value = input.value.trim();
-            
-            if (value !== "") {
-                if (parseInt(value) < 1) {
-                    input.value = "";
-                } else if (parseInt(value) > 100) {
-                    input.value = value.slice(0, -1);
-                }
-        }
-          
-            var input = document.getElementById("duzenleDersfinal");
-            var value = input.value.trim();
-            
-            if (value !== "") {
-                if (parseInt(value) < 1) {
-                    input.value = "";
-                } else if (parseInt(value) > 100) {
-                    input.value = value.slice(0, -1);
-                }
-        }
-        //girilen değer yok ise sonuç ekranını tamamen temizleme işlemi
-            var input1 = document.getElementById('duzenleDersvize');
-            var input2 = document.getElementById('duzenleDersfinal');
-            var sonuc = document.getElementById('duzenSonuc');
-          
-            if (input1.value || input2.value) {
-            var vize = document.getElementById("duzenleDersvize").value;
-            var final = document.getElementById("duzenleDersfinal").value;
-            var yuzdevize = document.getElementById("vize").value;
-            var yuzdefinal = document.getElementById("final").value;
-            var yuzde1 = (vize * yuzdevize) / 100;
-            var yuzde2 = (final * yuzdefinal) /100;
-            var sonuc = yuzde1 + yuzde2;
-            var sonuc = sonuc.toFixed(1);
-            document.getElementById("duzenSonuc").innerHTML = sonuc;
-
-            document.getElementById("duzenSonuc").style.color = sonuc < 60 ? 'red' : 'black';
-        
-            } else {
-                duzenSonuc.textContent = "";
-            }
+function duzenleHesap() {
+        sifirYuz()
+        islemler()
         }
     
-
-
 function dersadi() {
     const deger1 = document.getElementById("dersadi").value;
-
     if (deger1 !==""){
         saved.style.opacity=1;
     }
@@ -179,8 +78,7 @@ function dersadi() {
         saved.style.opacity=0.5;
     }
 }
-
-     //ekrandaki öğlerin hareketini kapatma işlemi
+//ekrandaki öğlerin hareketini kapatma işlemi
 function preventScroll(event) 
 {event.preventDefault();}
 document.addEventListener('touchmove', preventScroll, { passive: false });
@@ -189,4 +87,3 @@ function moveCursorToEnd(inputElement) {
     var length = inputElement.value.length;
     inputElement.setSelectionRange(length, length);
 }
-
